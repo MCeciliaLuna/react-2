3046de4d9b2d9545cde8a4097a75db531b203004
MCeciliaLuna <lunama.cecilia@gmail.com>
2022-05-11 16:39:14 -0300

    primeras prácticas de react router dom

--- src/index.js
+++ src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ErrorPage from './pages/ErrorPage';
+import Home from './pages/Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App />} />
+      <Route path='/404' element={<ErrorPage />} />
+      <Route path='/home' element={<Home />} />
+    </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
