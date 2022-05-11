bb796ed3ef834520b64c24db72ee81fc19d3093a
MCeciliaLuna <lunama.cecilia@gmail.com>
2022-05-11 15:56:50 -0300

    repaso react: props y desestructuración de datos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import './App.css';
-import Navbar from './components/Navbar/Navbar';
-import Footer from './components/Footer/Footer';
-import axios from 'axios';
-import { useState } from 'react';
-import CardPersonajes from './components/CardPersonajes/CardPersonajes';
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import CardPersonajes from "./components/CardPersonajes/CardPersonajes";
 
 function App() {
+  const [personajes, setPersonajes] = useState([]);
 
-  const [personajes, setPersonajes] = useState({})
+  const obtenerPersonajes = async () => {
+    const resp = await axios.get("https://rickandmortyapi.com/api/character");
+    setPersonajes(resp.data.results);
 
-  const obtenerPersonajes = async() => {
-    const resp = await axios.get('https://rickandmortyapi.com/api/character')
-    setPersonajes(resp.data.results)
-  }
+    const { name, id, url } = resp.data.results[0]; // desestructuré datos del objeto, convirtiendolo en una variable para manipularlos afuera
+
+    console.log(name);
+    console.log(id);
+    console.log(url);
+  };
+
+  useEffect(() => {
+    //controla el renderizado del componente (los estados)
+    obtenerPersonajes(); //
+  }, []); //[] "se esperana ' " dice en error de consola
 
   return (
     <div>
-    <Navbar />
-    <button onClick={obtenerPersonajes}>Consumir API</button>
-    <CardPersonajes />
-    <Footer />
+      <Navbar />
+      <main>
+        <button onClick={obtenerPersonajes}>Consumir API</button>
+        <section className="d-flex flex-wrap">
+          {personajes.map((personaje) => (
+            <CardPersonajes
+              key={personaje.id}
+              image={personaje.image}
+              name={personaje.name}
+              species={personaje.species}
+              url={personaje.url}
+            />
+          ))}
+        </section>
+      </main>
+      <Footer />
     </div>
   );
 }
--- src/components/CardPersonajes/CardPersonajes.js
+++ src/components/CardPersonajes/CardPersonajes.js
@@ -1,18 +1,16 @@
 import React from "react";
 
-export default function CardPersonajes(props) {
+export default function CardPersonajes({image,name,species,url}) { //se desestructura props para no poner (props), se ponen las propiedades directamente, las contienen las llaves
   return (
-    <div>
-      <div className="card">
-        <img src={props.image} className="card-img-top" alt="..." />
+      <div className="card col-3">
+        <img src={image} className="card-img-top" alt="..." />
         <div className="card-body">
-          <h5 className="card-title">{props.name}</h5>
-          <p className="card-text">{props.species}</p>
-          <a href={props.url} className="btn btn-primary">
+          <h5 className="card-title">{name}</h5>
+          <p className="card-text">{species}</p>
+          <a href={url} className="btn btn-primary">
             Go somewhere
           </a>
         </div>
       </div>
-    </div>
   );
 }
